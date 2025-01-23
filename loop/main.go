bec0931aa45fee6d4faf14e7bebca411a569d527
wolf <stefan.wolf@heinze.de>
2025-01-23 11:32:42 +0100

    added a while loop

--- loop/main.go
+++ loop/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Printf("i = %v \n", i)
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Printf("i = %v \n", i)
+	// }
+
+	// for i := 0; i < 42; i++ {
+	// 	x := rand.Intn(5)
+
+	// 	if x%2 == 0 {
+	// 		fmt.Printf("Iteration: %v \t x: %v, is even \n", i, x)
+	// 	} else {
+	// 		fmt.Printf("Iteration: %v \t x: %v, is odd \n", i, x)
+	// 	}
+	// }
+
+	y := 0
+
+	for y < 10 {
+		fmt.Printf("Iteration: %v \n", y)
+		y++
 	}
 }
