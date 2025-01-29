6f32a2be673c63b2bd90f7498240f43072c392e1
wolf <stefan.wolf@heinze.de>
2025-01-29 13:27:57 +0100

    Concurrency and race condition

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"runtime"
 	"sync"
 	"time"
 )
@@ -34,27 +33,53 @@ func main() {
 	}
 
 	saySomething(p1)
-	seconds := rand.Intn(1)
+	seconds := rand.Intn(5)
 	go func() {
 		time.Sleep(time.Second * time.Duration(seconds))
-		fmt.Println("#1 I needed", (time.Second*time.Duration(seconds)) + 1, "seconds")
+		fmt.Println("#1 I needed", (time.Second * time.Duration(seconds)), "seconds")
 		wg.Done()
 	}()
 
 	go func() {
 		time.Sleep(time.Second * time.Duration(seconds))
-		fmt.Println("#2 I needed", (time.Second*time.Duration(seconds)) + 1, "seconds")
+		fmt.Println("#2 I needed", (time.Second * time.Duration(seconds)), "seconds")
 		wg.Done()
 	}()
+	/*
+		var counter int
+
+		// intentionally created a race condition
+		for i := 0; i < 100; i++ {
+			go func() {
+				v := counter
+				runtime.Gosched()
+				v++
+				counter = v
+				fmt.Println(counter)
+				wg.Done()
+			}()
+		}
+
+		// fix with the atomic package
+		var counter2 int64
+		for i := 0; i < 100; i++ {
+			go func() {
+				atomic.AddInt64(&counter2, 1)
+				fmt.Println(counter2)
+				wg.Done()
+			}()
+		}
+	*/
+	// fix with mutex 
+	var mu sync.Mutex
 	var counter int
-
-	// intentionally created a race condition
 	for i := 0; i < 100; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
-			runtime.Gosched()
 			v++
 			counter = v
+			mu.Unlock()
 			fmt.Println(counter)
 			wg.Done()
 		}()
