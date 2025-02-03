bd48a12f89adbf4ac0d274d2dee2a50988fb3d9f
wolf <stefan.wolf@heinze.de>
2025-02-03 15:10:06 +0100

    added files

--- TestingHandOnExercise/main.go
+++ TestingHandOnExercise/main.go
@@ -26,4 +26,18 @@ func main() {
 	for k, v := range word.UseCount(quote.SunAlso) {
 		fmt.Println(v, k)
 	}
+
+	xxi := gen()
+	for _, v := range xxi {
+		fmt.Println(mymath.CenteredAvg(v))
+	}
+}
+
+func gen() [][]int {
+	a := []int{1, 4, 6, 8, 100}
+	b := []int{0, 8, 10, 1000}
+	c := []int{9000, 4, 10, 8, 6, 12}
+	d := []int{123, 744, 140, 200}
+	e := [][]int{a, b, c, d}
+	return e
 }
